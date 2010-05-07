b46851e4f34be516967e34dfdce17f421cb319e1
sinsoku <sinsoku.listy@gmail.com>
2010-05-07 11:10:35 +0900

    web上のサンプルを使用
    ウノウラボ Unoh Labs: Pythonでメールを送信したい人のためのサンプル集
    http://labs.unoh.net/2007/06/python_2.html

--- src/tracreportmail.py
+++ src/tracreportmail.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-print 'hello, world'
+# -*- coding: utf-8 -*-
+import smtplib
+from email.MIMEText import MIMEText
+from email.Utils import formatdate
+
+def create_message(from_addr, to_addr, subject, body):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Date'] = formatdate()
+    return msg
+
+def send(from_addr, to_addr, msg):
+    # SMTPの引数を省略した場合はlocalhost:25
+    s = smtplib.SMTP()
+    s.sendmail(from_addr, [to_addr], msg.as_string())
+    s.close()
+
+if __name__ == '__main__':
+    from_addr = 'spam@example.com'
+    to_addr = 'egg@example.com'
+    msg = create_message(from_addr, to_addr, 'test subject', 'test body')
+    send(from_addr, to_addr, msg)
