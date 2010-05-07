9d94f4d85aae367b433fd1edda061f976ab83d36
sinsoku <sinsoku.listy@gmail.com>
2010-05-07 11:57:50 +0900

    日本語メッセージ対応、複数宛先対応

--- src/tracreportmail.py
+++ src/tracreportmail.py
@@ -3,36 +3,37 @@
 # -*- coding: utf-8 -*-
 import smtplib
 from email.MIMEText import MIMEText
+from email.Header import Header
 from email.Utils import formatdate
 
-def create_message(from_addr, to_addr, subject, body, encoding):
+def create_message(from_addr, to_addrs, subject, body, encoding):
     # 'text/plain; charset="encoding"'というMIME文書を作ります
-    msg = MIMEText(body, 'plain', encoding)
-    msg['Subject'] = Header(subject, encoding)
+    msg = MIMEText(body.encode(encoding), 'plain', encoding)
+    msg['Subject'] = Header(subject.encode(encoding), encoding)
     msg['From'] = from_addr
-    msg['To'] = to_addr
+    msg['To'] = ', '.join(to_addrs)
     msg['Date'] = formatdate()
     return msg
 
-def send(from_addr, to_addr, msg):
+def send(from_addr, to_addrs, msg):
     # SMTPの引数を省略した場合はlocalhost:25
     s = smtplib.SMTP()
-    s.sendmail(from_addr, [to_addr], msg.as_string())
+    s.sendmail(from_addr, to_addrs, msg.as_string())
     s.close()
 
-def send_via_gmail(from_addr, to_addr, msg):
+def send_via_gmail(from_addr, to_addrs, msg):
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.ehlo()
     s.starttls()
     s.ehlo()
     s.login('yourname@gmail.com', 'password')
-    s.sendmail(from_addr, [to_addr], msg.as_string())
+    s.sendmail(from_addr, to_addrs, msg.as_string())
     s.close()
 
 if __name__ == '__main__':
     from_addr = 'spam@example.com'
-    to_addr = 'egg@example.com'
-    msg = create_message(from_addr, to_addr, u'テスト', u'本文', 'ISO-2022-JP')
-    send(from_addr, to_addr, msg)
-    send_via_gmail(from_addr, to_addr, msg)
+    to_addrs = ['egg@examplex.com']
+    msg = create_message(from_addr, to_addrs, u'テスト', u'本文', 'ISO-2022-JP')
+    send(from_addr, to_addrs, msg)
+    send_via_gmail(from_addr, to_addrs, msg)
 
